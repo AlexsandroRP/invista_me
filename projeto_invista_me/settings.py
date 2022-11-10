01734cded22f3c43b90cd11db8a435f1156460b1
alexsandrorp.py <alexsandrorp.py@gmail.com>
2022-11-09 18:56:22 -0600

    v1.2

--- projeto_invista_me/settings.py
+++ projeto_invista_me/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ebvwz_^nh62$#k=uacn!e4)x0q)mr2*xdex6oxg$052qg!n5ka'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['invista-me2.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
